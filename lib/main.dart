9252e378b4e4c4c686326f262fc67ec248c93bb7
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-08 22:43:10 +0530

    change stateful to stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,24 +5,12 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Alert Generator'),
-          backgroundColor: Colors.red,
-          leading: Icon(Icons.warning_amber_rounded,size: 30.0,)
-          ,),
-      ),
-    );
+    return Container();
   }
+}
 }
