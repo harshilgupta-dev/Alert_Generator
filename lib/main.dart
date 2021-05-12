51f7adcb5907e315fa3881f8acb97ddcf6119e08
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-12 20:23:20 +0530

    AlertButtons done

--- lib/main.dart
+++ lib/main.dart
@@ -66,27 +66,47 @@ class MyApp extends StatelessWidget {
                   );
                 },
               ),
-              Builder(builder: (context){
-                return RaisedButton(
-                  child: Text('Alert Buttons'),
-                  onPressed: (){
-                  Alert(
-                    type: AlertType.warning,
-                    title: 'Alert with Buttons',
-                    desc: 'Alert with multiple buttons',
-                    context: context,
-                    buttons: [
-                      DialogButton(child: Text(' FLAT',style: TextStyle(color: Colors.white, fontSize: 20)),
-                          onPressed: (){
-                            return Navigator.pop(context);
-                          },
-                        color: Color.fromRGBO(0, 179, 134, 1.0),
-                      ),
-                    ]
-                  ).show();
+              Builder(
+                builder: (context) {
+                  return RaisedButton(
+                    child: Text('Alert Buttons'),
+                    onPressed: () {
+                      Alert(
+                          type: AlertType.warning,
+                          title: 'Alert with Buttons',
+                          desc: 'Alert with multiple buttons',
+                          context: context,
+                          buttons: [
+                            DialogButton(
+                              child: Text(' FLAT',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20)),
+                              onPressed: () {
+                                return Navigator.pop(context);
+                              },
+                              color: Color.fromRGBO(0, 179, 134, 1.0),
+                            ),
+                            DialogButton(
+                                child: Text(
+                                  'Gradient',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                                onPressed: () {
+                                  return Navigator.pop(context);
+                                },
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color.fromRGBO(116, 116, 118, 1.0),
+                                    Color.fromRGBO(52, 118, 118, 1.0)
+                                  ]
+                                ),
+                            ),
+                          ]).show();
+                    },
+                  );
                 },
-                );
-              },)
+              )
             ],
           ),
         ),
