32b62bcdd4ddccbd1f5921909369db607d82098b
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-12 23:55:52 +0530

    add IntrinsicWidth widget

--- lib/main.dart
+++ lib/main.dart
@@ -21,179 +21,183 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Center(
-          child: Column(
-            children: [
-              Builder(
-                builder: (context) {
-                  return RaisedButton(
-                    onPressed: () {
-                      Alert(
-                              context: context,
-                              title: 'Basic Alert',
-                              desc: 'It generate basic alert')
-                          .show();
-                    },
-                    child: Text('Basic Alert'),
-                  );
-                },
-              ),
-              Builder(
-                builder: (context) {
-                  return RaisedButton(
-                    onPressed: () {
-                      Alert(
+          child: IntrinsicWidth(
+            stepWidth: 100.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Builder(
+                  builder: (context) {
+                    return RaisedButton(
+                      onPressed: () {
+                        Alert(
+                                context: context,
+                                title: 'Basic Alert',
+                                desc: 'It generate basic alert')
+                            .show();
+                      },
+                      child: Text('Basic Alert'),
+                    );
+                  },
+                ),
+                Builder(
+                  builder: (context) {
+                    return RaisedButton(
+                      onPressed: () {
+                        Alert(
+                            context: context,
+                            type: AlertType.error,
+                            title: 'Alert With Button',
+                            desc: 'It generate Alert Along a button',
+                            buttons: [
+                              DialogButton(
+                                child: Text(
+                                  'Close It',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 25.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                onPressed: () {
+                                  return Navigator.pop(context);
+                                },
+                              )
+                            ]).show();
+                      },
+                      child: Text('Alert Button'),
+                    );
+                  },
+                ),
+                Builder(
+                  builder: (context) {
+                    return RaisedButton(
+                      child: Text('Alert Buttons'),
+                      onPressed: () {
+                        Alert(
+                            type: AlertType.warning,
+                            title: 'Alert with Buttons',
+                            desc: 'Alert with multiple buttons',
+                            context: context,
+                            buttons: [
+                              DialogButton(
+                                child: Text(' FLAT',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20)),
+                                onPressed: () {
+                                  return Navigator.pop(context);
+                                },
+                                color: Color.fromRGBO(0, 179, 134, 1.0),
+                              ),
+                              DialogButton(
+                                child: Text(
+                                  'Gradient',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                                onPressed: () {
+                                  return Navigator.pop(context);
+                                },
+                                gradient: LinearGradient(colors: [
+                                  Color.fromRGBO(116, 116, 118, 1.0),
+                                  Color.fromRGBO(52, 118, 118, 1.0)
+                                ]),
+                              ),
+                            ]).show();
+                      },
+                    );
+                  },
+                ),
+                Builder(
+                  builder: (context) {
+                    return RaisedButton(
+                      child: Text('Alert With Style'),
+                      onPressed: () {
+                        Alert(
                           context: context,
-                          type: AlertType.error,
-                          title: 'Alert With Button',
-                          desc: 'It generate Alert Along a button',
+                          title: 'Alert Style',
+                          desc: 'Alert With Style',
+                          style: alertStyle(),
+                          type: AlertType.info,
                           buttons: [
                             DialogButton(
-                              child: Text(
-                                'Close It',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 25.0,
-                                  color: Colors.white,
+                                child: Text(
+                                  'Cool',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20.0),
+                                ),
+                                onPressed: () {
+                                  return Navigator.pop(context);
+                                },
+                                width: 150.0,
+                                color: Colors.red,
+                                //highlightColor: Colors.black,
+                                //splashColor: Colors.black,
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color.fromRGBO(116, 116, 52, 1.0),
+                                    Color.fromRGBO(50, 150, 191, 1.0)
+                                  ],
+                                ),
+                                radius: BorderRadius.circular(20.0))
+                          ],
+                        ).show();
+                      },
+                    );
+                  },
+                ),
+                Builder(builder: (context){
+                  return RaisedButton(
+                      child: Text('Alert with customImage'),
+                      onPressed: (){
+                        Alert(
+                          context: context,
+                          title: 'Alert',
+                          desc: 'Alert with custom Image',
+                          image: Image.asset('images/temp.png')
+                        ).show();
+                  });
+                }),
+                Builder(builder: (context){
+                  return RaisedButton(
+                      child: Text('Alert with Custom Content'),
+                      onPressed: (){
+                        Alert(
+                          context: context,
+                          title: 'LOGIN PAGE',
+                          type: AlertType.success,
+                          content: Column(
+                            children: [
+                              TextField(
+                                decoration: InputDecoration(
+                                  icon: Icon(Icons.account_circle,),
+                                  labelText: 'Username'
                                 ),
                               ),
-                              onPressed: () {
-                                return Navigator.pop(context);
-                              },
-                            )
-                          ]).show();
-                    },
-                    child: Text('Alert Button'),
-                  );
-                },
-              ),
-              Builder(
-                builder: (context) {
-                  return RaisedButton(
-                    child: Text('Alert Buttons'),
-                    onPressed: () {
-                      Alert(
-                          type: AlertType.warning,
-                          title: 'Alert with Buttons',
-                          desc: 'Alert with multiple buttons',
-                          context: context,
-                          buttons: [
-                            DialogButton(
-                              child: Text(' FLAT',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20)),
-                              onPressed: () {
-                                return Navigator.pop(context);
-                              },
-                              color: Color.fromRGBO(0, 179, 134, 1.0),
-                            ),
-                            DialogButton(
-                              child: Text(
-                                'Gradient',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
-                              ),
-                              onPressed: () {
-                                return Navigator.pop(context);
-                              },
-                              gradient: LinearGradient(colors: [
-                                Color.fromRGBO(116, 116, 118, 1.0),
-                                Color.fromRGBO(52, 118, 118, 1.0)
-                              ]),
-                            ),
-                          ]).show();
-                    },
-                  );
-                },
-              ),
-              Builder(
-                builder: (context) {
-                  return RaisedButton(
-                    child: Text('Alert With Style'),
-                    onPressed: () {
-                      Alert(
-                        context: context,
-                        title: 'Alert Style',
-                        desc: 'Alert With Style',
-                        style: alertStyle(),
-                        type: AlertType.info,
-                        buttons: [
-                          DialogButton(
-                              child: Text(
-                                'Cool',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20.0),
-                              ),
-                              onPressed: () {
-                                return Navigator.pop(context);
-                              },
-                              width: 150.0,
-                              color: Colors.red,
-                              //highlightColor: Colors.black,
-                              //splashColor: Colors.black,
-                              gradient: LinearGradient(
-                                colors: [
-                                  Color.fromRGBO(116, 116, 52, 1.0),
-                                  Color.fromRGBO(50, 150, 191, 1.0)
-                                ],
-                              ),
-                              radius: BorderRadius.circular(20.0))
-                        ],
-                      ).show();
-                    },
-                  );
-                },
-              ),
-              Builder(builder: (context){
-                return RaisedButton(
-                    child: Text('Alert with customImage'),
-                    onPressed: (){
-                      Alert(
-                        context: context,
-                        title: 'Alert',
-                        desc: 'Alert with custom Image',
-                        image: Image.asset('images/temp.png')
-                      ).show();
-                });
-              }),
-              Builder(builder: (context){
-                return RaisedButton(
-                    child: Text('Alert with Custom Content'),
-                    onPressed: (){
-                      Alert(
-                        context: context,
-                        title: 'LOGIN PAGE',
-                        type: AlertType.info,
-                        content: Column(
-                          children: [
-                            TextField(
+                              TextField(
+                              obscureText: true,
                               decoration: InputDecoration(
-                                icon: Icon(Icons.account_circle,),
-                                labelText: 'Username'
-                              ),
-                            ),
-                            TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                icon: Icon(Icons.keyboard),
-                                labelText: 'Password',
+                                  icon: Icon(Icons.keyboard),
+                                  labelText: 'Password',
 
-                              ),
-                            )
-                          ],
-                        ),
-                        buttons: [
-                          DialogButton(child: Text('Login'),
-                              onPressed: (){
-                                return Navigator.pop(context);
-                              }
+                                ),
                               )
-                        ]
-                      ).show();
-                });
-              })
-            ],
+                            ],
+                          ),
+                          buttons: [
+                            DialogButton(child: Text('Login'),
+                                onPressed: (){
+                                  return Navigator.pop(context);
+                                }
+                                )
+                          ]
+                        ).show();
+                  });
+                })
+              ],
+            ),
           ),
         ),
       ),
